041f84141fe75948f07745de2020277181af0759
iwswordpress <iwswordpress@gmail.com>
2020-11-14 23:51:13 +0000

    added stock

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -12,11 +12,13 @@ class App extends Component {
             first='Craig'
             last='West'
           ></my-component>
+          <uc-stock-price stock-symbol='AAPL'></uc-stock-price>
+          <iws-test app='using iwswordpress-test'></iws-test>
           <app-home
             style={{ color: 'green' }}
             app='UsefulServices.co.uk'
           ></app-home>
-          <img src={logo} className='App-logo' alt='logo' />
+          {/* <img src={logo} className='App-logo' alt='logo' /> */}
         </header>
       </div>
     );
