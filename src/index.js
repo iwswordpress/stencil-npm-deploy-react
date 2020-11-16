13b3428fc61e4132fb538bcb3a6b0773a0892ece
iwswordpress <iwswordpress@gmail.com>
2020-11-16 11:50:12 +0000

    added both stock price and stock finder

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { defineCustomElements } from 'iwswordpress-test/loader';
+// import web components
+import { defineCustomElements } from 'iwswordpress-web/loader';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
 
+//hook up custom elements to app
 defineCustomElements(window);
